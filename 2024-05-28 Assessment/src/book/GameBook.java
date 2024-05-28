33e42e5c1b38a45099246fafc886ee2c19273c18
zy <kinomaxxing@gmail.com>
2024-05-28 11:34:02 +0100

    update

--- 2024-05-28 Assessment/src/book/GameBook.java
+++ 2024-05-28 Assessment/src/book/GameBook.java
@@ -15,4 +15,39 @@ public class GameBook {
         endings = new HashMap<>();
         bookSections = new HashMap<>();
     }
+
+    public void addSection(AbstractBookSection section) throws DuplicateSectionReferenceException {
+        int sectionNumber = section.getSectionNumber();
+        if (bookSections.containsKey(sectionNumber)) {
+            AbstractBookSection existingSection = bookSections.get(sectionNumber);
+            if (!existingSection.getText().equals(section.getText())) {
+                throw new DuplicateSectionReferenceException(
+                        "Section already exists within the book with different text!", sectionNumber);
+            }
+        }
+
+        if (section.isEndingSection()) {
+            endings.put(sectionNumber, section);
+        }
+        bookSections.put(sectionNumber, section);
+
+    }
+
+    public void setStartingSection(AbstractBookSection start) throws DuplicateSectionReferenceException {
+        int sectionNumber = start.getSectionNumber();
+        if (start.getSectionNumber() != 1) {
+            throw new InvalidSectionReferenceException("Starting section must have the value 1!", sectionNumber);
+        }
+
+        if (bookSections.containsKey(sectionNumber)) {
+            AbstractBookSection existingSection = bookSections.get(sectionNumber);
+            if (!existingSection.getText().equals(start.getText())) {
+                throw new DuplicateSectionReferenceException(
+                        "Starting section exists with different text!", sectionNumber);
+            }
+        }
+
+        this.start = start;
+        bookSections.put(sectionNumber, start);
+    }
 }
