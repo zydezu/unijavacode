dcdbbdf89cb1e5cf3a7777ee1416e4ad550e28f2
zy <kinomaxxing@gmail.com>
2024-05-28 12:03:59 +0100

    yay

--- 2024-05-28 Assessment/src/book/InvalidSectionReferenceException.java
+++ 2024-05-28 Assessment/src/book/InvalidSectionReferenceException.java
@@ -2,7 +2,6 @@ package book;
 
 public class InvalidSectionReferenceException extends RuntimeException {
     int sectionReference;
-
     public InvalidSectionReferenceException(String message, int sectionReference) {
         super(message);
         this.sectionReference = sectionReference;
